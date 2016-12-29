8358dbd8c0a737add253b93b4c4a042d7960aca5
Ken Luy <Ken@Kens-MacBook-Pro-2.local>
2016-12-29 07:09:22 -0800

    project creation

---
+++ js/firstComponent.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox.
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('content')
+);
