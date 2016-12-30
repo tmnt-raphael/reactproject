99589bac6a364d95df7b7819f2ec6f0beb14494f
Ken Luy <Ken@Kens-MacBook-Pro-2.local>
2016-12-29 18:26:09 -0800

    No longer importing component

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-import FirstComponent from '/FirstComponent'
+//import FirstComponent from '/FirstComponent'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox.
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('content')
+);
