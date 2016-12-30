fb4ca2ffb413ce771c96fa33debaae89c95e53ae
Ken Luy <Ken@Kens-MacBook-Pro-2.local>
2016-12-29 21:20:27 -0800

    fixed import statement

--- js/app.js
+++ js/app.js
@@ -1,18 +1,1 @@
-//import FirstComponent from '/FirstComponent'
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-var CommentBox = React.createClass({
-  render: function() {
-    return (
-      <div className="commentBox">
-        Hello, world! I am a CommentBox.
-      </div>
-    );
-  }
-});
-
-ReactDOM.render(
-  <CommentBox />,
-  document.getElementById('content')
-);
+import FirstComponent from './FirstComponent'
